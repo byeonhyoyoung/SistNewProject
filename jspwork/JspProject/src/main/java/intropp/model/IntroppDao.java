b574ecdc6693a92d4e656db2139d4fc7265ee5b8
hyoyoung <gydud0109@naver.com>
2024-03-07 17:40:11 +0900

    day0307

--- jspwork/JspProject/src/main/java/intropp/model/IntroppDao.java
+++ jspwork/JspProject/src/main/java/intropp/model/IntroppDao.java
@@ -1,5 +1,74 @@
 package intropp.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Vector;
+
+import mysql.db.DbConnect;
+
 public class IntroppDao {
 
+	DbConnect db=new DbConnect();
+	
+	public void introppInsert(IntroppDto dto)
+	{
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		
+		String sql="insert into intropp values(null,?,?,?,?,?,?)";
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			
+			
+			pstmt.setString(1, dto.getName());
+			pstmt.setString(2, dto.getAge());
+			pstmt.setString(3, dto.getBirthday());
+			pstmt.setString(4, dto.getHometown());
+			pstmt.setString(5, dto.getHobby());
+			pstmt.setString(6, dto.getMemo());
+			
+			pstmt.execute();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(pstmt, conn);
+		}
+		
+	}
+	
+	//전체조회
+	public List<IntroppDto> getAllDatas()
+	{
+		List<IntroppDto> list=new Vector<IntroppDto>();
+		
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		String sql="select * from intropp order by num";
+		
+		
+			try {
+				pstmt=conn.prepareStatement(sql);
+				rs=pstmt.executeQuery();
+				
+				while(rs.next())
+				{
+					IntroppDto dto=new IntroppDto();
+					
+					
+					
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+	}
 }
