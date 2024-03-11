dae5094a2d237144f6db329917bb38cf05c7db5f
hyoyoung <gydud0109@naver.com>
2024-03-11 17:21:27 +0900

    day0311

--- jspwork/JspProject/src/main/java/intropp/model/IntroppDao.java
+++ jspwork/JspProject/src/main/java/intropp/model/IntroppDao.java
@@ -11,48 +11,46 @@ import mysql.db.DbConnect;
 
 public class IntroppDao {
 
-	DbConnect db=new DbConnect();
+	DbConnect db = new DbConnect();
 	
-	public void introppInsert(IntroppDto dto)
+	//생성
+	public void insertIntropp(IntroppDto dto)
 	{
 		Connection conn=db.getConnection();
 		PreparedStatement pstmt=null;
 		
-		String sql="insert into intropp values(null,?,?,?,?,?,?)";
+		String sql = "insert into intropp values(null,?,?,?,?,?,?)";
 		
-		try {
-			pstmt=conn.prepareStatement(sql);
+			try {
+				pstmt=conn.prepareStatement(sql);
+				
+				pstmt.setString(1, dto.getName());
+				pstmt.setString(2, dto.getAge());
+				pstmt.setString(3, dto.getBirthday());
+				pstmt.setString(4, dto.getHometown());
+				pstmt.setString(5, dto.getHobby());
+				pstmt.setString(6, dto.getMemo());
+				
+				pstmt.execute();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally {
+				db.dbClose(pstmt, conn);
+			}
 			
-			
-			pstmt.setString(1, dto.getName());
-			pstmt.setString(2, dto.getAge());
-			pstmt.setString(3, dto.getBirthday());
-			pstmt.setString(4, dto.getHometown());
-			pstmt.setString(5, dto.getHobby());
-			pstmt.setString(6, dto.getMemo());
-			
-			pstmt.execute();
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			db.dbClose(pstmt, conn);
-		}
-		
 	}
 	
-	//전체조회
+	//전제조회
 	public List<IntroppDto> getAllDatas()
 	{
-		List<IntroppDto> list=new Vector<IntroppDto>();
+		List<IntroppDto> list = new Vector<IntroppDto>();
 		
 		Connection conn=db.getConnection();
 		PreparedStatement pstmt=null;
 		ResultSet rs=null;
 		
-		String sql="select * from intropp order by num";
-		
+		String sql = "select * from intropp order by num=?";
 		
 			try {
 				pstmt=conn.prepareStatement(sql);
@@ -62,18 +60,20 @@ public class IntroppDao {
 				{
 					IntroppDto dto=new IntroppDto();
 					
-					dto.setNum(rs.getString("num"));
 					dto.setName(rs.getString("name"));
 					
-					
-					
-					
-					
 				}
+				
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+				
+
 			}
+			return list;
 			
+			
+		
 	}
+	
 }
