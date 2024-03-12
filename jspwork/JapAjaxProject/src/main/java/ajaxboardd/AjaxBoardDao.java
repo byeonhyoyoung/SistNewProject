a0b6c2461bc8863af1058c8c0b94cbff8a7d4c48
hyoyoung <gydud0109@naver.com>
2024-03-12 18:53:26 +0900

    day0312

--- jspwork/JapAjaxProject/src/main/java/ajaxboardd/AjaxBoardDao.java
+++ jspwork/JapAjaxProject/src/main/java/ajaxboardd/AjaxBoardDao.java
@@ -114,36 +114,7 @@ public class AjaxBoardDao {
 			return dto;
 			
 	}
-	
-	//수정
-	public void updateBoard(AjaxBoardDto dto)
-	{
-		Connection conn=db.getConnection();
-		PreparedStatement pstmt=null;
 		
-		String sql = "update ajaxboard set writer=?, subject=?, avata=?, content=? where num=?";
-		
-			try {
-				pstmt=conn.prepareStatement(sql);
-				
-				pstmt.setString(1, dto.getWriter());
-				pstmt.setString(2, dto.getSubject());
-				pstmt.setString(3, dto.getAvata());
-				pstmt.setString(4, dto.getContent());
-				pstmt.setString(5, dto.getNum());
-				
-				
-				pstmt.execute();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}finally {
-				db.dbClose(pstmt, conn);
-			}
-			
-	}
-	
-	
 	//삭제
 	public void deleteBoard(String num)
 	{
@@ -165,6 +136,33 @@ public class AjaxBoardDao {
 				db.dbClose(pstmt, conn);
 			}
 			
+			
+	}
+	
+	//수정
+	public void updateBoard(AjaxBoardDto dto)
+	{
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		
+		String sql = "update ajaxboard set writer=?, subject=?, avata=?, content=? where num=?";
+		
+			try {
+				pstmt=conn.prepareStatement(sql);
+				
+				pstmt.setString(1, dto.getWriter());
+				pstmt.setString(2, dto.getSubject());
+				pstmt.setString(3, dto.getAvata());
+				pstmt.setString(4, dto.getContent());
+				pstmt.setString(5, dto.getNum());
+				
+				pstmt.execute(); //실행해주기
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally {
+				db.dbClose(pstmt, conn);
+			}
 			
 	}
 }
